82364d9e34cd0df97068bdbad4a85a89a83b30cb
yosuke0315 <wonderfulworld0315@gmail.com>
2016-03-30 15:28:22 +0900

    initial commit

---
+++ lib/itamae/plugin/recipe/yk_mecab.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/yk_mecab/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module YkMecab
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/yk_mecab/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module YkMecab
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
