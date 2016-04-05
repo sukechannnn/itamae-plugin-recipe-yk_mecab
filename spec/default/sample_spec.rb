a9ea6352a91dff7b0276a9abe6b5f31c281e9769
yosuke0315 <wonderfulworld0315@gmail.com>
2016-04-05 15:41:42 +0900

    mod serverspec and comment

--- spec/default/sample_spec.rb
+++ spec/default/sample_spec.rb
@@ -1,46 +1,37 @@
 require 'spec_helper'
 
-%w{gcc-c++ wget make}.each do |pkg|
+%w{wget make}.each do |pkg|
   describe package(pkg) do
     it { should be_installed }
   end
 end
 
-describe package('ruby') do
+describe package('gcc-c++'), if: ['redhat', 'fedora', 'amazon'].include?(os[:family]) do
   it { should be_installed }
+end
+
+describe package('g++'), if: ['debian', 'ubuntu'].include?(os[:family]) do
+  it { should be_installed }
+end
+
+describe command('ruby -v') do
+  let(:disable_sudo) { true }
+  its(:exit_status) { should eq 0 }
 end
 
 describe package('nkf') do
   it { should be_installed }
 end
 
-describe package('mecab-0.994') do
-  it { should be_installed }
+describe file('/usr/local/bin/mecab') do
+  it { should be_file }
+  it { should be_executable }
 end
 
-# describe package('httpd'), :if => os[:family] == 'redhat' do
-#   it { should be_installed }
-# end
-#
-# describe package('apache2'), :if => os[:family] == 'ubuntu' do
-#   it { should be_installed }
-# end
-#
-# describe service('httpd'), :if => os[:family] == 'redhat' do
-#   it { should be_enabled }
-#   it { should be_running }
-# end
-#
-# describe service('apache2'), :if => os[:family] == 'ubuntu' do
-#   it { should be_enabled }
-#   it { should be_running }
-# end
-#
-# describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
-#   it { should be_enabled }
-#   it { should be_running }
-# end
-#
-# describe port(80) do
-#   it { should be_listening }
-# end
+describe file('/usr/local/lib/mecab/dic/ipadic/') do
+  it { should be_directory }
+end
+
+describe file('/home/vagrant/custom.dic') do
+  it { should be_file }
+end
